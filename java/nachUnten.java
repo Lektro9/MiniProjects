5308459b0f4406603d18d28d6cf0c77ac430a0ae
Lektro9 <49228244+Lektro9@users.noreply.github.com>
2019-04-03 12:05:15 +0200

    Delete nachUnten.java
    Not a project.

--- java/nachUnten.java
+++
@@ -1,8 +0,0 @@
-public class nachUnten extends Thread{
-
-    public void run (){
-        for (int i = 100; i >-1 ; i--) {
-            System.out.println("      " + i);        
-        }
-    }
-}
